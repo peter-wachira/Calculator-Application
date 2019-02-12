13d3310d740b35f0383e0f1652020b070cc1b3cb
pwachira900@gmail.com <pwachira900@gmail.com>
2019-02-12 15:23:02 +0300

    created project folders and files , js folder, read me , index page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var remainder = function(number1,number2){
+  number1 = parseInt(prompt("enter no."));
+
+  number2 = parseInt(prompt("enter no.2"));
+
+ return number1 % number2;
+}
+
+alert(remainder());
